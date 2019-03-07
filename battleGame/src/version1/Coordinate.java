1796f5517cdb90d676f492b5e9514eebd7268148
maryam <ismailova.maryam@gmail.com>
2019-03-07 13:15:05 +0400

    add Coordinate class methods

--- battleGame/src/version1/Coordinate.java
+++ battleGame/src/version1/Coordinate.java
@@ -10,7 +10,18 @@ public class Coordinate {
 	}
 	
 	public Coordinate(String coord) {
-		this.y=((int)coord.charAt(0))-((int)'A');
-		this.x=Character.getNumericValue(coord.charAt(1));
+		this.x=((int)coord.charAt(0))-((int)'A');
+		this.y=Character.getNumericValue(coord.charAt(1));
+	}
+	public static Coordinate stringToCoordinate(String coord) {
+		Coordinate c=new Coordinate(coord);
+		return c;
+	}
+	public static String coordinateToString(Coordinate c) {
+		String s="";
+		s+=(char)((int)'A'+c.x);
+		s+=String.valueOf(c.y);
+		System.out.println("Coordinate: "+s);
+		return s;
 	}
 }
